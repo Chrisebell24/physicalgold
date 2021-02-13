42c8e7d1960cad287f0f05cb7e5e400abfe1d28c
Chrisebell24 <31806186+Chrisebell24@users.noreply.github.com>
2021-02-13 14:38:18 -0600

    Added fast load

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import pickle
 import os
 import flask
 from flask import Flask, send_from_directory
@@ -14,12 +15,26 @@ app = dash.Dash(__name__, server=server)
 app.title='GOLD'
 
 server = app.server
+
+DIRNAME = os.path.dirname(os.path.abspath(__file__))
+
 app.layout = html.Div([
+
+    html.Div([
+        html.Button(id='fake_button'),
+    ],style={'display': 'none'}),
+
     html.H1('Gold Best Physical Price'),
+    
     
     html.Div([
         dcc.Interval(id='interval_pull', interval=600000),
         html.Div('Last Update:'),
+        
+        html.Div(
+            id='fast_load', 
+        style={'display': 'inline-block'}),
+        
         html.Div(id='last_update'),
     ]),
     
@@ -36,7 +51,7 @@ def favicon():
     
 @app.server.route('/apple-touch-icon.png')
 def applePng():
-  return send_from_directory(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'assets'),'apple-touch-icon.png', mimetype='image/png')
+  return send_from_directory(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'assets'),'apple-icon.png', mimetype='image/png')
 
 STATIC_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'assets')
 @server.route('/assets/<resource>')
@@ -45,26 +60,62 @@ def serve_static(resource):
     
 
 @app.callback(
+    
+    Output('fast_load','children'),
+    [
+        Input('fake_button', 'n_clicks'),
+    ],
+)
+def fast_load_table(fake_button):
+
+    print('running fast load')
+    fp  = os.path.join(DIRNAME, 'gold_table.p')
+    
+    if os.path.exists(fp):
+        print('reading from file')
+        
+        data = pickle.load(open( fp, mode='rb' ) )
+        df = data['df']
+        last_update = data['last_update']
+        tbl = [
+            html.Div(last_update),
+            html_table(df, 'title'),
+        ]
+        
+    else:
+        tbl = []
+        
+        
+    print('finished fast load')
+    return tbl
+
+@app.callback(
     [
         Output('tbl_gold', 'children'),
         Output('last_update', 'children'),
+        Output('fast_load', 'style'),
     ],
     [
         Input('interval_pull', 'n_intervals'),
     ],
 )
 def download_gold_data(interval_pull):
-    data, cols = [], []
-    
-    
-        
-        
+    print('running long pull')
     df = get_all(cash_back=0)
     tbl = html_table(df, 'title')
     last_update_datetime = pd.datetime.today()
-        
-        
-    return tbl, last_update_datetime
+    fp  = os.path.join(DIRNAME, 'gold_table.p')
+    data = {
+        'df': df,
+        'last_update': last_update_datetime,
+    }
+    pickle.dump(data, open( fp, mode='wb' ))
+    style = {'display': 'none'}
+    print('finished long pull')
+    return tbl, last_update_datetime, style
+
+
+
 
 if __name__ == '__main__':
-    app.run_server()
+    app.run_server(debug=False)
